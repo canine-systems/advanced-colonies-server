bf7aba41c2e48af29a91f0457c0e80ead7b07646
Ellen Marie Dash <me@duckie.co>
2026-01-09 23:45:25 -0500

    add maintenance/sync-map.sh.

---
+++ maintenance/sync-map.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+BACKUP_REMOTE="puppy@map.canine.systems"
+
+# Configure it so the TERM process calls `exit 1` at the top level.
+trap "exit 1" TERM
+export TOP_PID=$$
+
+START=$(date +'%d %b %Y at %H:%M:%S')
+START_TS=$(date +'%s')
+
+LOG_FILE="$HOME/backup.log"
+LAST_BACKUP_FILE="$HOME/.last-backup.txt"
+
+function fail() {
+    echo "$@" > ~/.backup-error.txt
+    printf "\n-----------------------------" >> $LOG_FILE
+    echo >> $LOG_FILE
+    echo "$@" >> $LOG_FILE
+    notify-send -u critical "Backup failed!" "See $LOG_FILE for details."
+    # Kill the top-level process, instead of just exiting this function.
+    kill -s TERM $TOP_PID
+}
+
+function notify-log() {
+    notify-send "$1" "$2"
+    printf "\n-----------------------------" >> $LOG_FILE
+    echo >> $LOG_FILE
+    echo "$1" >> $LOG_FILE
+    printf "$2\n" >> $LOG_FILE
+}
+
+[ -f "$LOG_FILE" ] && mv "$LOG_FILE" "$LOG_FILE.1"
+printf "Backup started on $START\n\n" > $LOG_FILE
+
+if [ -z "${HOSTNAME}" ]; then
+    fail '$HOSTNAME is empty.'
+fi
+
+# -a:   "Archive mode."
+# -z:   Compress file data during transfer
+# -v:   Increase verbosity.
+# -P:   --partial (keep partially-transferred files), --progress (show progress during transfer)
+# -h:   Use human-readable numbers.
+# --delete: Delete extraneous files from dest dirs.
+{ rsync -avzPh --delete "$HOME/server/squaremap/web/" "${BACKUP_REMOTE}:/usr/share/caddy/" 2>&1 || fail "Error running rsync ($?)."; } | tee -a $LOG_FILE
+
+END_TS=$(date +'%s')
+
+DURATION=$[$END_TS - $START_TS]
+
+rm -f ~/.backup-error.txt
+
+echo "$END_TS" > $LAST_BACKUP_FILE
+
+echo | tee -a $LOG_FILE
+ssh "${BACKUP_REMOTE}" df -h / | tee -a $LOG_FILE
+
+notify-log "Backup complete." "Started on $START.\\nTook $DURATION seconds."
