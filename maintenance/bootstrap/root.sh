77afa5c870fe76567bf9cb151a02705e3a128b9f
Ellen Marie Dash <me@duckie.co>
2026-01-11 01:46:03 -0500

    add more output for main bootstrap script

--- maintenance/bootstrap/root.sh
+++ maintenance/bootstrap/root.sh
@@ -5,10 +5,14 @@ function run() {
     "$@" || exit $?
 }
 
-cd
+echo "=== Advanced Colonies Minecraft Server Setup ==="
+
+echo "1. Installing Java, tmux, and htop."
 run apt install default-jre tmux htop
+echo "2. Creating Minecraft user."
 run useradd -r -m -U -d /opt/minecraft -s /bin/bash minecraft
 
+echo "3. Fetching and running bootstrap/minecraft.sh"
 run wget -O /tmp/bootstrap-minecraft.sh https://github.com/canine-systems/advanced-colonies-server/raw/refs/heads/main/maintenance/bootstrap/minecraft.sh
 run su -l minecraft bash /tmp/bootstrap-minecraft.sh
 run rm /tmp/bootstrap-minecraft.sh
