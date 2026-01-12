087b068c024eb938b9e2b553e9b70d6c3ec91f2a
Ellen Marie Dash <me@duckie.co>
2026-01-11 22:04:48 -0500

    make bootstrap script install rsync

--- bootstrap/bootstrap-root.sh
+++ bootstrap/bootstrap-root.sh
@@ -7,8 +7,8 @@ function run() {
 
 echo "=== Advanced Colonies Minecraft Server Setup ==="
 
-echo "1. Installing Java, tmux, htop, and unzip."
-run apt install -y default-jre tmux htop unzip
+echo "1. Installing required packages."
+run apt install -y default-jre tmux htop unzip rsync
 echo "2. Creating Minecraft user."
 run useradd -r -m -U -d /opt/minecraft -s /bin/bash minecraft
 
