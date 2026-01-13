8a6bc6810d5969da54391365b1f00c57af61cb7e
Ellen Marie Dash <me@duckie.co>
2026-01-12 20:48:45 -0500

    make update.sh rsync-based.

--- maintenance/update.sh
+++ maintenance/update.sh
@@ -1,12 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
+function run() {
+    echo "> $@"
+    "$@" || exit $?
+}
+
+if [ "$1" = "--dry-run" ]; then
+    DRYRUN="--dry-run"
+fi
+
+TMPDIR="$HOME/serverpack-update"
 FILE="$HOME/serverpack-auto-update.zip"
 
 if [ -f "$FILE" ]; then
     rm "$FILE" || exit $?
 fi
-wget https://github.com/canine-systems/advanced-colonies-server/releases/latest/download/advanced-colonies-serverpack.zip -O "$FILE" || exit $?
-unzip -l "$FILE" || exit $?
+
+run rm -rf "$TMPDIR"
+run mkdir "$TMPDIR"
+cd "$TMPDIR"
+
+run wget https://github.com/canine-systems/advanced-colonies-server/releases/latest/download/advanced-colonies-serverpack.zip -O "$FILE"
+run unzip -l "$FILE"
 read -p 'Press enter to continue, or Ctrl-c to abort.'
-cd "$HOME/server"
-unzip "$FILE" || exit $?
+
+run unzip "$FILE"
+
+FILES=$(/usr/bin/ls -1 | grep -v squaremap)
+
+run rsync $DRYRUN -avhc --progress --delete $FILES squaremap/*.* "$HOME/server/"
