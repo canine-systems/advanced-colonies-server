cf7b1b464bdd74bc3fcc99b94c7f8fa96d4672c5
Ellen Marie Dash <me@duckie.co>
2026-02-03 20:45:05 -0500

    cache pakku.jar in .cache/ instead of .downloads/

--- bin/update-pakku.sh
+++ bin/update-pakku.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-mkdir -p .downloads || exit $?
-wget https://github.com/juraj-hrivnak/Pakku/releases/latest/download/pakku.jar -O .downloads/pakku.jar || exit $?
+mkdir -p .cache || exit $?
+wget https://github.com/juraj-hrivnak/Pakku/releases/latest/download/pakku.jar -O .cache/pakku.jar || exit $?
